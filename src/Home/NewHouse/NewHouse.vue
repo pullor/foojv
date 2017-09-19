dbbbe0cac6a79a2de56d7be8a59d34af9eb683e2
zjl516088421 <516088421@qq.com>
2017-09-19 16:27:59 +0800

    完善footerbar

--- src/Home/NewHouse/NewHouse.vue
+++ src/Home/NewHouse/NewHouse.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    NewHouse
+  <div class="newhouse-box">
+    <Swipe :auto="4000" style="width:100%;height:10rem">
+      <Swipe-item style="">1</Swipe-item>
+      <Swipe-item style="">2</Swipe-item>
+      <Swipe-item style="">3</Swipe-item>
+    </Swipe>
+    
   </div>
 </template>
 
 <script>
-  export default{}
+  import { Swipe, SwipeItem } from 'mint-ui'
+  export default{
+    components: {
+      Swipe,
+      SwipeItem
+    }
+  }
 </script>
 
 <style>
+.newhouse-box{
+  width:100%;
+}
 
 </style>
--- src/components/Footbar.vue
+++ src/components/Footbar.vue
@@ -1,31 +1,70 @@
 <template>
   <section id="footerbar">
-    <router-link to="/home">首页</router-link>
-    <router-link to="/tools">工具</router-link>
-    <router-link to="/watch">看房</router-link>
-    <router-link to="/mine">我</router-link>
+    <router-link to="/home">
+      <i class="tabar-nav-i first-nav-i"><img src="../assets/tabar-home1.png" width="100%" alt=""></i>
+      <i class="tabar-nav-i second-nav-i"><img src="../assets/tabar-home2.png" width="100%" alt=""></i>
+      首页
+    </router-link>
+    <router-link to="/tools">
+      <i class="tabar-nav-i first-nav-i"><img src="../assets/tabar-tool1.png" width="100%" alt=""></i>
+      <i class="tabar-nav-i second-nav-i"><img src="../assets/tabar-tool2.png" width="100%" alt=""></i>
+      工具
+    </router-link>
+    <router-link to="/watch">
+      <i class="tabar-nav-i first-nav-i"><img src="../assets/tabar-watch1.png" width="100%" alt=""></i>
+      <i class="tabar-nav-i second-nav-i"><img src="../assets/tabar-watch2.png" width="100%" alt=""></i>
+      看房
+    </router-link>
+    <router-link to="/mine">
+      <i class="tabar-nav-i first-nav-i"><img src="../assets/tabar-my1.png" width="100%" alt=""></i>
+      <i class="tabar-nav-i second-nav-i"><img src="../assets/tabar-my2.png" width="100%" alt=""></i>
+      我
+    </router-link>
   </section>
 </template>
 
 <script>
-export default {
+  export default {
 
-}
+  }
 </script>
 
 <style>
-#footerbar{
-  position: fixed;
-  bottom: 0px;
-  display: flex;
-  width: 100%;
-  text-align: center;
-  background-color: #ddd;
-}
-#footerbar a{
-  flex: 1;
-  text-decoration: none;
-  color: #999;
-  font-size: 1.3rem;
-}
+  #footerbar{
+    position: fixed;
+    bottom: 0px;
+    display: flex;
+    width: 100%;
+    text-align: center;
+    /*background-color: #ddd;*/
+    border-top: 1px solid #ccc;
+    padding-top: 0.2rem;
+  }
+  #footerbar a{
+    flex: 1;
+    text-decoration: none;
+    color: #999;
+    font-size: 1rem;
+  }
+  #footerbar .first-nav-i{
+    display: none;
+  }
+  .router-link-active .first-nav-i{
+    color: red !important;
+    display: block !important;
+  }
+  .router-link-active .second-nav-i{
+    display: none;
+  }
+  .router-link-active {
+    color: red !important;
+  }
+  .tabar-nav-i {
+    /*border: 1px solid red;*/
+    width: 1.8rem;
+    height: 1.8rem;
+    display: block;
+    margin: 0rem auto;
+  }
 </style>
+
